0aebe0b35757a639568b6d0cc8e11e72333d7ab6
Michael Nenn <mnenn@illumina.com>
2020-02-18 11:53:54 -0800

    Validate nucleobase.

--- Rosalind-Problems/CountDnaNucleotides.fs
+++ Rosalind-Problems/CountDnaNucleotides.fs
@@ -10,15 +10,20 @@ module Tests =
 
     let toUpper (x:string) = x.ToUpper()
 
-    let explode (x:string) = [| for c in x -> c |]
+    let validNucleobase c =
+        match c with
+        | ('A'|'C'|'G'|'T') -> c
+        | _ -> ' '
     
-    let getCharacter (c,_) = c 
+    let explode (x:string) = [| for c in x -> validNucleobase(c) |]
+    
+    let getNucleobase (nb,_) = nb 
 
-    let countBases data =
+    let countNucleoBases data =
         toUpper data
         |> explode
         |> Seq.countBy id
-        |> Seq.sortBy(fun x -> getCharacter(x))
+        |> Seq.sortBy(fun x -> getNucleobase(x))
         |> Seq.toList
 
     let getCount (_,c) = c
@@ -27,17 +32,26 @@ module Tests =
         do new Converter(output) |> Console.SetOut
 
         [<Fact>]
-        member __.``Count occurrences of bases`` () =
+        member __.``Count occurrences of nucleobases`` () =
             // Arrange.
-            let inputFile = "/Users/mnenn/Downloads/rosalind_dna.txt"
+            let workingDir = Path.Combine(Directory.GetCurrentDirectory(), "TestData")
+            let inputFile = Path.Combine(workingDir, "rosalind_dna.txt")
             let data = File.ReadAllText(inputFile).Trim()
             
             // Act.
-            let results = countBases data
+            let results = countNucleoBases data
             
             // Assert
             let output = results |> List.map(fun x -> getCount(x).ToString() + " ") |> String.Concat
-            let outputFile = "/Users/mnenn/Downloads/rosalind_out.txt"
+            let outputFile = Path.Combine(workingDir, "rosalind_out.txt")
             if File.Exists(outputFile) then File.Delete(outputFile)
             File.WriteAllText(outputFile, output)
             Assert.Equal(4, results.Length)
+            for t in results do
+                match getNucleobase(t) with
+                | 'A' -> Assert.Equal(20, getCount(t))
+                | 'C' -> Assert.Equal(12, getCount(t))
+                | 'G' -> Assert.Equal(17, getCount(t))
+                | 'T' -> Assert.Equal(21, getCount(t))
+                | _ -> Assert.False(true)
+        
