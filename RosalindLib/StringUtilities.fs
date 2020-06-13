1ebf4398584ea4aa5c0d7d28da12620a295498d7
Mitch Nenn <mitch.nenn@earthlink.net>
2020-06-12 21:16:05 -0700

    Translating RNA.

--- RosalindLib/StringUtilities.fs
+++ RosalindLib/StringUtilities.fs
@@ -1,9 +1,14 @@
 namespace RosalindLib
 
+open System
+
 module StringUtilities =
     
     let charCount x = Seq.filter ((=) x) >> Seq.length        
 
+    let strCompareIgnoreCase a1 a2 =
+        0 = String.Compare(a1, a2, StringComparison.InvariantCultureIgnoreCase)
+    
     let chunkString size str  =
          let rec loop (s:string) accum =
              let branch = size < s.Length
