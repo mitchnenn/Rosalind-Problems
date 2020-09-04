2f9fcd974581849f1a7e260049c7ce7e1e478ab0
Mitch Nenn <mitch.nenn@earthlink.net>
2020-09-03 18:07:21 -0700

    Mendels second law.

---
+++ Rosalind-Problems/MendelSecondLaw.fs
@@ -0,0 +1,33 @@
+namespace Rosalind_Problems
+
+open System
+open Xunit
+open Xunit.Abstractions
+open Helpers
+open RosalindLib.MathUtilities
+
+module MendelSecondLaw =
+    
+    type MendelSecondLawTests(output: ITestOutputHelper) =
+        do new Converter(output) |> Console.SetOut    
+
+        let singleProb i pop =
+            let f1 = double(factorial pop)
+            let f2 = double(factorial i)
+            let f3 = double(factorial (pop - i))
+            let p1 = pown (double 0.25) (int(i))
+            let p2 = pown (double 0.75) (int(pop - i))
+            (f1 / (f2 * f3)) * p1 * p2 
+        
+        let sumProbability N pop : double = 
+            seq { N .. pop + 1 }
+            |> Seq.map(fun i -> (singleProb (bigint(i)) (bigint(pop))))
+            |> Seq.sum
+        
+        [<Fact>]
+        member __.``Mendels law of independence test`` () =
+            let k = 5 // generation
+            let N = 8 // least number of AaBb in the generation    
+            let P = pown 2 k // population of generation
+            printfn "%.3f" (sumProbability N P)
+            Assert.True(true)
--- RosalindLib/MathUtilities.fs
+++ RosalindLib/MathUtilities.fs
@@ -5,3 +5,7 @@ module MathUtilities =
     let roundFloat (digits:int) (value:float)
         = System.Math.Round(value, digits)
 
+    let rec factorial x:bigint =
+        if x < bigint(1) then bigint(1)
+        else x * factorial (x - bigint(1))
+    
