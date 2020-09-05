209a2bb68b8e558d0fec618a511bc4b8ac723a50
Mitch Nenn <mitch.nenn@earthlink.net>
2020-09-04 20:36:04 -0700

    Was working, now not working.

---
+++ ProteinMotifFinder/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.Text.RegularExpressions
+open FSharp.Data
+
+let toMatch (aMatch:Match) = aMatch 
+
+[<EntryPoint>]
+let main argv =
+    let name = "B5ZC00"
+    let url = sprintf "https://www.uniprot.org/uniprot/%s.fasta" name
+    let filedata = Http.RequestString(url)
+    let data = Regex.Replace(filedata.Substring(filedata.IndexOf('\n')), @"\t|\n|\r", "")
+    let matchColl = Regex.Matches(data, @"(?=(N[^P][ST][^P]))")
+    let matches = matchColl |> Seq.cast |> Seq.map (fun (r:Match) -> r)
+    
+                
+    0 // return an integer exit code
