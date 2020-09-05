2d8fad5bd151df273b9b9b127a9b85459afbb51c
Mitch Nenn <mitch.nenn@earthlink.net>
2020-09-04 21:24:50 -0700

    Don't output if no matches.

--- ProteinMotifFinder/Program.fs
+++ ProteinMotifFinder/Program.fs
@@ -2,6 +2,7 @@
 
 open System.Text.RegularExpressions
 open FSharp.Data
+open System.Linq
 
 let getSequence name = 
     let url = sprintf "https://www.uniprot.org/uniprot/%s.fasta" name
@@ -21,9 +22,10 @@ let formatIndexes indexes = indexes |> Seq.iter (printf "%A ")
 
 [<EntryPoint>]
 let main argv =
-    let name = "B5ZC00"
+    let name = "A2Z669"
     let matches = getMatches name
-    let indexes = matches |> Seq.map toIndex
-    printfn "%s" name
-    formatIndexes indexes         
+    if matches.Any() then
+        let indexes = matches |> Seq.map toIndex
+        printfn "%s" name
+        formatIndexes indexes         
     0 // return an integer exit code
