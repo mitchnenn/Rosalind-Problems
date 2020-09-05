573a00dec5cc92392b2331f3afb72324cd49a248
Mitch Nenn <mitch.nenn@earthlink.net>
2020-09-04 21:16:07 -0700

    Now it works.

--- ProteinMotifFinder/Program.fs
+++ ProteinMotifFinder/Program.fs
@@ -3,16 +3,27 @@
 open System.Text.RegularExpressions
 open FSharp.Data
 
-let toMatch (aMatch:Match) = aMatch 
+let getSequence name = 
+    let url = sprintf "https://www.uniprot.org/uniprot/%s.fasta" name
+    let filedata = Http.RequestString(url)
+    Regex.Replace(filedata.Substring(filedata.IndexOf('\n')), @"\t|\n|\r", "")
+
+let toMatch (aMatch:Match) = aMatch
+
+let getMatches name =
+    let data = getSequence name
+    let matchColl = Regex.Matches(data, @"(?=(N[^P][ST][^P]))")
+    matchColl |> Seq.cast |> Seq.map toMatch 
+
+let toIndex (aMatch:Match) = aMatch.Index + 1
+
+let formatIndexes indexes = indexes |> Seq.iter (printf "%A ")
 
 [<EntryPoint>]
 let main argv =
     let name = "B5ZC00"
-    let url = sprintf "https://www.uniprot.org/uniprot/%s.fasta" name
-    let filedata = Http.RequestString(url)
-    let data = Regex.Replace(filedata.Substring(filedata.IndexOf('\n')), @"\t|\n|\r", "")
-    let matchColl = Regex.Matches(data, @"(?=(N[^P][ST][^P]))")
-    let matches = matchColl |> Seq.cast |> Seq.map (fun (r:Match) -> r)
-    
-                
+    let matches = getMatches name
+    let indexes = matches |> Seq.map toIndex
+    printfn "%s" name
+    formatIndexes indexes         
     0 // return an integer exit code
