a9830dddc74ab878d138e67accd28743de9743b5
Mitch Nenn <mitch.nenn@earthlink.net>
2020-09-13 11:31:07 -0700

    So can match DNA codons, but incorrectly.

--- OpenReadFrames/Program.fs
+++ OpenReadFrames/Program.fs
@@ -1,22 +1,28 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+open System.Collections.Generic
 open FSharp.Data
 open RosalindLib
-open NucleotideBase
 
-type RnaCodon = CsvProvider<"TestData/rna-codon-table.csv">
+type DnaCodon = CsvProvider<"TestData/dna-codon-table.csv">
 
-let translateDnaToRna (dnaSequences:((string * string) list)) =
-    dnaSequences
-    |> List.map (fun s -> (fst(s), translateDnaToRna(snd(s))))
+let getDnaCodons = DnaCodon.Load("TestData/dna-codon-table.csv").Rows
+                |> Seq.map(fun x -> x.DnaCodon, x.AminoAcid)
+                |> dict
+
+let getAminoAcid dnaCodon (dnaCodons:IDictionary<string,string>) =
+    dnaCodons.Item dnaCodon
 
 [<EntryPoint>]
 let main argv =
-    let rnaCodons = RnaCodon.Load("TestData/rna-codon-table.csv")
+    let dnaCodons = getDnaCodons
     let dnaSequences = ParseFasta.parseFastaEntries "TestData/dna-sequence.fasta"
-    let rnaStrings = translateDnaToRna dnaSequences
     
-    printfn "%A" dnaSequences
-    printfn "%A" rnaStrings
-    
+    let aminoAcids = dnaSequences
+                     |> Seq.map (fun (k,s) -> k, StringUtilities.chunkString 3 s)
+                     |> Seq.map (fun (k,a) -> k, a |> List.map(fun s -> getAminoAcid s dnaCodons))
+                     |> Seq.map (fun (k,a) -> k, a |> String.concat "")
+                     |> Seq.map (fun (k,s) -> k, s.Split "Stop")
+    printfn "%A" aminoAcids
+        
     0 // return an integer exit code
