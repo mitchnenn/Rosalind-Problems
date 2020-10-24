4c535621f05c7ee5a209a3b96dffde8719d82e8e
Mitch Nenn <mitch.nenn@earthlink.net>
2020-10-24 07:19:36 -0700

    Kmers permutations.

--- Rosalind-Problems/EnumKmersLex.fs
+++ Rosalind-Problems/EnumKmersLex.fs
@@ -1,40 +1,34 @@
 namespace Rosalind_Problems
 namespace Rosalind_Problems
 
-open System.Text
 open Xunit
 open Xunit.Abstractions
-open FParsec
-
-type KmersSymbolDefRecord = {Symbols:char list; Permutations:int}
-
-module EnumKmersLex =
-    
-    type EnumKmersLexTests(output:ITestOutputHelper) =
-        
-        let symbol =  letter .>> spaces            
-        
-        let symbols = many symbol
-        
-        let permutations = pint32
-        
-        let parseKmersSymbols = many (tuple2 symbols permutations)
-        
-        let getKmerSymbolDef (path:string) =
-            let reply = runParserOnFile parseKmersSymbols () path Encoding.UTF8
-            match reply with
-            | Success(result,_,_) -> result |> List.map(fun t -> {Symbols=(fst t);Permutations=(snd t)})
-            | Failure(_,_,_) -> List.empty
-
+ 
+module AlphabetPermutations =
+    type AlphabetPermutationsTests (output:ITestOutputHelper) =
+         
+        let concatString (strings:string list) (alphabet:string list) = seq {
+            for s in strings |> List.sort do
+                for a in alphabet |> List.sort do
+                    yield s + a
+        }
+ 
+        let concatStringList (strings:string list) (alphabet:string list) =
+            concatString strings alphabet |> Seq.toList
+                    
+        let getPermutationsByLength (alphabet:string list) (length:int) =
+            let rec recFunc strings len =
+                match len < length with
+                | true -> recFunc (concatStringList strings alphabet) (len + 1)
+                | false -> strings
+            recFunc alphabet 1
+         
         [<Fact>]
-        let ``Enumerate kmers lex test`` () =
-            // Arrange.
-            let kmerSymbolDef = getKmerSymbolDef "TestData/EnumKmersLexData.txt"
-            output.WriteLine(sprintf "%A" kmerSymbolDef)
-            // Act.
-            let actual = ""
-            // Assert.
-            let expected = "AA\nAC\nAG\nAT\nCA\nCC\nCG\nCT\nGA\nGC\nGG\nGT\nTA\nTC\nTG\nTT\n"
-            //Assert.Equal(expected, actual)
-            Assert.True(true)
+        let ``Alphabet Permutations Test`` () =
+            let alphabet = ["A"; "B"; "C"; "D"; "E"; "F"; "G"; "H"] 
+            let length = 3
+            let alphabetPermutations = getPermutationsByLength alphabet length
+            alphabetPermutations |> List.iter(fun i -> output.WriteLine(sprintf "%s" i))
+            Assert.True(true);
+             
         
